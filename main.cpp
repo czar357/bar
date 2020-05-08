c0905ce7d8a64731397da1b95075d063c0a21427
Joao Otavio Turatti Barbosa <joaootavio22017@gmail.com>
2020-05-08 00:46:19 -0300

    Projeto Criado

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{   string d;
+    int a,b,c;
+    double e;
+
+    cout << "Insira seu sexo: (F ou M)" << endl;
+    cin>>d;
+    if(d = "F"|| d = "f"){
+        cout<<"a";
+    }
+    return 0;
+}
