97c41c37ed6359a48284c1fca347aa475f8c1638
Joao Otavio Turatti Barbosa <joaootavio22017@gmail.com>
2020-05-08 01:36:10 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {   string d;
     int a,b,c;
-    double e;
-
+    double e, f, h;
+    cout<<fixed<<setprecision(2);
     cout << "Insira seu sexo: (F ou M)" << endl;
     cin>>d;
     cout << "Insira a quantidade de cervejas consumidas: " << endl;
@@ -16,6 +16,25 @@ int main()
     cout << "Insira a quantidade de espetinhos consumidos: " << endl;
     cin>>c;
 
+    f = (a*5.0)+(b*3.0)+(c*7.0);
 
+    cout<<endl<<"RELATORIO"<<endl;
+    cout<<endl<<"Consumo = R$ "<<f<<endl;
+    if(f<=30.0){
+        cout<<"Couvert = R$ 4.00"<<endl;
+        e =4.00;}
+    else{
+        cout<<"Couvert = R$ 0.00"<<endl;
+        e=0;
+    }
+   if (d=="F"){
+        h=8.0;
+        cout<<"Ingresso = R$ 8.00"<<endl;
+    }
+    if (d == "M"){
+        h=10.0;
+        cout<<"Ingresso = R$ 10.00"<<endl;
+    }
+    cout<<endl<<"Valor a pagar: R$"<< e + f + h<<endl;
     return 0;
 }
